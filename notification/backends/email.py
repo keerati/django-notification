063b54ddfc425783d70fc29ad46e56b3507ffacc
keerati <keerati@acommerce.asia>
2014-12-18 15:23:31 +0700

    Add new email backend to support template from database

--- notification/backends/email.py
+++ notification/backends/email.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.template import Template
 from django.utils.translation import ugettext
 
 from notification import backends
+import sys
+import logging
 
 
 class EmailBackend(backends.BaseBackend):
@@ -40,3 +43,23 @@ class EmailBackend(backends.BaseBackend):
         }, context)
 
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [recipient.email])
+
+class EmailBackendWithNoticeTemplate(EmailBackend):
+    def deliver(self, recipient, sender, notice_type, extra_context):
+        from notification.models import NoticeTemplate
+        context = self.default_context()
+        context.update({
+            "recipient": recipient,
+            "sender": sender,
+            "notice": ugettext(notice_type.display),
+        })
+        context.update(extra_context)
+        try:
+            template_obj = NoticeTemplate.objects.get(notice_type = notice_type)  
+            subject = template_obj.subject
+            body_text = Template(template_obj.body_text).render(context)
+            body_html = Template(template_obj.body_html).render(context)
+            send_mail(subject, body_text, settings.DEFAULT_FROM_EMAIL, [recipient.email], html_message=body_html)
+        except:
+            logging.debug("error sending email", exc_info=sys.exc_info())
+
