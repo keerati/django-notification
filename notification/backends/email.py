7c9b499dd9dc1a50724625706810dbf924d6b7bc
keerati <keerati@acommerce.asia>
2014-12-29 15:24:57 +0700

    Remove custom email backend

--- notification/backends/email.py
+++ notification/backends/email.py
@@ -43,23 +43,3 @@ class EmailBackend(backends.BaseBackend):
         }, context)
 
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [recipient.email])
-
-class EmailBackendWithNoticeTemplate(EmailBackend):
-    def deliver(self, recipient, sender, notice_type, extra_context):
-        from notification.models import NoticeTemplate
-        context = self.default_context()
-        context.update({
-            "recipient": recipient,
-            "sender": sender,
-            "notice": ugettext(notice_type.display),
-        })
-        context.update(extra_context)
-        try:
-            template_obj = NoticeTemplate.objects.get(notice_type = notice_type)  
-            subject = template_obj.subject
-            body_text = Template(template_obj.body_text).render(context)
-            body_html = Template(template_obj.body_html).render(context)
-            send_mail(subject, body_text, settings.DEFAULT_FROM_EMAIL, [recipient.email], html_message=body_html)
-        except:
-            logging.debug("error sending email", exc_info=sys.exc_info())
-
