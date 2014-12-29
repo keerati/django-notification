1ebefda1a281774178b6257417d92f4ecfced371
keerati <keerati@acommerce.asia>
2014-12-29 15:04:25 +0700

    Forgot to remove admin stuff

--- notification/admin.py
+++ notification/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from notification.models import NoticeType, NoticeSetting, NoticeQueueBatch, NoticeTemplate
+from notification.models import NoticeType, NoticeSetting, NoticeQueueBatch
 
 
 class NoticeTypeAdmin(admin.ModelAdmin):
@@ -10,10 +10,6 @@ class NoticeTypeAdmin(admin.ModelAdmin):
 class NoticeSettingAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "notice_type", "medium", "send"]
 
-class NoticeTemplateAdmin(admin.ModelAdmin):
-    list_display = ["notice_type", "subject"]
-
 admin.site.register(NoticeQueueBatch)
 admin.site.register(NoticeType, NoticeTypeAdmin)
 admin.site.register(NoticeSetting, NoticeSettingAdmin)
-admin.site.register(NoticeTemplate, NoticeTemplateAdmin)
